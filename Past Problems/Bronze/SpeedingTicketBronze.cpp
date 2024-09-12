506adaffaed21bf785ff68a905fd9bf702d602af
Zafir <shamsizafir@gmail.com>
2024-09-11 23:30:01 -0400

    day 3: 2.5 hrs, 1 proble, started another

---
+++ Past Problems/Bronze/SpeedingTicketBronze.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(){
+    
+
+
+
+
+}
--- Past Problems/Bronze/TheCowSignalBronze.cpp
+++ Past Problems/Bronze/TheCowSignalBronze.cpp
@@ -2,20 +2,39 @@
 #include <cstdio>
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-
-
 int main() {
+    
+    freopen("cowsignal.in", "r", stdin);
+    freopen("cowsignal.out", "w", stdout);  
     int row, column, magnify;
     scanf("%d %d %d", &row, &column, &magnify);
 
-    vector<string>strings;
+    string line;
+    char thechar;
+    string outline;
 
-    for(int p=0; p<row; p++){
-        scanf("%s", &strings[p]);
+    for (int i = 0; i < row; i++) {
+        cin >> line;  // Change 2: Using 'cin >> line' instead of getline
+
+       
+        for (int j = 0; j < column; j++) {
+            thechar = line[j];  // Access the correct character from line (use j instead of column)
+            for (int x = 0; x < magnify; x++) {
+                outline.push_back(thechar);  // Append magnified character
+            }
+        }
+
+        // Print the magnified line multiple times for vertical magnification
+        for (int m = 0; m < magnify; m++) {
+            cout << outline << endl;  
+        }
+
+        outline.clear();  
     }
 
-    cout<<strings[0];
+    return 0;
 }
