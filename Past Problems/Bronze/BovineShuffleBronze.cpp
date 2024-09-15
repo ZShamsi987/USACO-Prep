87a2f775c06781f9c26c15370cd110042342ab19
Zafir <shamsizafir@gmail.com>
2024-09-14 23:57:12 -0400

    Day 5: 1 hour, 1 solution im cooked

--- Past Problems/Bronze/BovineShuffleBronze.cpp
+++ Past Problems/Bronze/BovineShuffleBronze.cpp
@@ -1,15 +1,42 @@
+//only passed 1 test case!! :( not sure rn will check
+
 #include <algorithm>
 #include <cstdio>
 #include <iostream>
 #include <cstdlib>
-
+#include <vector>
 using namespace std;
 
 int main(){
-    
+    freopen("shuffle.in", "r", stdin);
+    freopen("shuffle.out", "w", stdout);  
+
+    int N;
+    scanf("%d", &N);
+
+    vector<int> unsortedorder(N);
+    vector<int> unsortedcows(N);
+    vector<int> realorder(N);
+
+    int z;
+    int x;
+
+    for(int i = 0; i < N; i++){
+        scanf("%d", &unsortedorder[i]);
+    }
+
+    for(int k = 0; k < N; k++){
+        scanf("%d", &unsortedcows[k]);
+    }
+
+    for(int j = 0; j < N; j++){
+        z = unsortedorder[j];
+        x = unsortedcows[j];
+        realorder[z - 1] = x; 
+    }
 
 
-
-
-
+    for(int i = 0; i < N; i++){
+        cout << realorder[i] << endl;
+    }
 }
