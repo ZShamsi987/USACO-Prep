981f46ea87aa1796c38da06afed9d3fa85217fdb
Zafir <shamsizafir@gmail.com>
2024-09-17 22:06:44 -0400

    day 8: cold for past 2 days, highkey tweakin

--- Past Problems/Bronze/CircularBarnBronze.cpp
+++ Past Problems/Bronze/CircularBarnBronze.cpp
@@ -1,3 +1,15 @@
+/*
+-i was starting to get it lowk, but i dont ened the room capacity associated with each room like what i was tryna do
+-set a local tracker and acc tracker
+-goes through each barn(1 for loop) and then nest it simulating each cow going through it(2nd for loop)
+-find minimum at end of each main loop
+-find amount of moves because its circle so divide by %n
+-multiply position variable by amount of cows which is found by the vector that has each cow
+
+not hard, but bad implemntation overall, needa cook harder
+*/
+
+
 #include <algorithm>
 #include <cstdio>
 #include <iostream>
@@ -25,7 +37,7 @@ int main(){
         bool full;
 
         for(int w=0; w<N; w++){
-            
+
         }
 
 
