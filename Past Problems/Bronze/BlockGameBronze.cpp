0c63cc5fa882ae4da03eca607fcc2733bd734c40
Zafir <shamsizafir@gmail.com>
2024-10-02 23:27:25 -0400

    Day 23: 2 hours, still stuck

--- Past Problems/Bronze/BlockGameBronze.cpp
+++ Past Problems/Bronze/BlockGameBronze.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
-#include <utility>
 #include <vector>
-#include <cmath>
+#include <string>
 using namespace std;
 
-int main(){
-
-    int n; cin>>n;
-
-    vector<string>words(n*2);
-
-    for(int a=0; a<n*2; a+=2){
-        cin>>words[a]>>words[a+1];
+vector<int> countLetters(const string& word) { //takes input of string var word
+    vector<int> letterCount(26, 0);
+    for (char c : word) { //goes through each character putting it in lettercount
+        letterCount[c - 'a']++; //indexes it based on a/0 MOST IMPORTANT LINE
     }
-
-    //dont know how to complete all the possibilities of blocks
-    //thought of like an on/off thing for each block side but wasn't sure how to implement tbh
-
+    return letterCount;
 }
 
+int main() {
+    freopen("blocks.in", "r", stdin);
+    freopen("blocks.out", "w", stdout);  
+
+    int n; 
+    cin >> n;
+
+    vector<pair<string, string>> boards(n); //each block
+    vector<int> totalCounts(26, 0); //final letters
+    
+    for (int i = 0; i < n; i++) {
+        cin >> boards[i].first >> boards[i].second;
+
+        vector<int> count1 = countLetters(boards[i].first);
+        vector<int> count2 = countLetters(boards[i].second);
+
+        //each vector calls back count letters which returns the vector with all letter positions
+
+        for (int j = 0; j < 26; j++) {
+            totalCounts[j] += max(count1[j], count2[j]);  
+            //add to the running count whichever block is more needy on that letter
+        }
+    }
+
+    for (int j = 0; j < 26; j++) {
+        cout << totalCounts[j] << endl;
+    }
+
+    return 0;
+}
---
+++ Past Problems/Bronze/TeamTicTacToeBronze.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(){
+
+for(int i=0; i<3; i++){
+    string row;
+    getline(cin, row);
+    
+    
+}
+
+
+
+}
